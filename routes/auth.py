f0d9b23b7a36c398ea51e54973985d280da88bc1
T1agoESP <tiagofrauke@gmail.com>
2020-08-19 21:28:46 +0200

    Security + user checking

--- routes/auth.py
+++ routes/auth.py
@@ -27,34 +27,39 @@ def login():
 
 
 
-# Registro, check de disponivilidad de usuario, y generar token
+# Registro, check de disponibilidad de usuario, y generar token
 @app.route('/auth/register', methods=['POST'])
 def register():
     try:
         now = datetime.now()
         timestamp = now.strftime("%Y/%m/%d %H:%M:%S")
 
-        nick = request.args['nick']
-        email = request.args['email']
-        password = request.args['password']
+        nick = request.form.get('nick')
+        email = request.form.get('email')
+        password = request.form.get('password')
         ip = request.remote_addr
         fregistro = timestamp
         fultimaconex = timestamp
     except:
-        return jsonify({ 'msg': 'Error en los parametros', 'code':400 })
+        return jsonify({ 'msg': 'Error en los parametros', 'code':400 }), 400
 
     try:
         cur = mysql.connection.cursor()
+
+        cur.execute("SELECT * FROM usuarios WHERE nick='"+nick+"'")
+        num_rows = cur.fetchall()
+        if num_rows:
+            return jsonify({ 'msg': 'Error signing up due to duplicates', 'code':400 }), 400
+
         cur.execute("INSERT INTO usuarios (nick, email, password) VALUES (%s,%s,%s)", (nick, email, password))
         mysql.connection.commit()
-
-        cur.execute("INSERT INTO userinfo (ip, fecha_registro, fecha_ultima_conexion) VALUES (%s,%s,%s)", (ip, fregistro, fultimaconex))
+        cur.execute("INSERT INTO userinfo (ip, fecha_registro, fecha_ultima_conexion) VALUES (%s,%s,%s)", (ip,fregistro,fultimaconex))
         mysql.connection.commit()
         cur.close()
 
         date_time = now.strftime("%d/%m/%Y, %H:%M")
         print("\033[37m[\033[32m\033[01m+\033[0m\033[37m]\033[37m Nuevo usuario registrado \033[35m{"+nick+"} \033[37m("+date_time+")\033[0m")
-        return jsonify({ 'msg': 'Registro exitoso', 'token':'1234567890', 'code':200 })
+        return jsonify({ 'msg': 'Successful sign up', 'token':'1234567890', 'code':200 })
     except:
-        return jsonify({ 'msg': 'Error en el registro', 'code':400 })
+        return jsonify({ 'msg': 'Error signing up', 'code':400 }), 400
 
